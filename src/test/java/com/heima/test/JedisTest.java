5ff514009f99dcb982e9ebd565208846fed39f2b
itcast <hello@itcast.cn>
2023-02-04 13:50:38 +0800

    Redis环境配置

--- src/test/java/com/heima/test/JedisTest.java
+++ src/test/java/com/heima/test/JedisTest.java
@@ -1,10 +1,26 @@
 package com.heima.test;
 
 import org.junit.jupiter.api.Test;
+import redis.clients.jedis.Jedis;
 
+/*
+ * 使用Jedis获取Redis
+ * */
 public class JedisTest {
     @Test
-    public void testRedis(){
+    public void testRedis() {
+        //1.获取连接
+        Jedis jedis = new Jedis("localhost", 6379);
+        //2.执行具体操作
+        jedis.set("username", "lyz");
 
+        //jedis.del("username");
+
+        jedis.hset("myhash", "addr", "wanan");
+
+        String hValue = jedis.hget("myhash", "addr");
+        System.out.println(hValue);
+        //3.关闭连接
+        jedis.close();
     }
 }
